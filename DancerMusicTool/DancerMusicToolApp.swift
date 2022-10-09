07d8a91e41502fbbb03ecdd5555d553315c7d111
Alexandr Zhelanov <AlexandrZhelanov@gmail.com>
2022-10-09 22:08:09 +0300

    Initial commit

---
+++ DancerMusicTool/DancerMusicToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  DancerMusicToolApp.swift
+//  DancerMusicTool
+//
+//  Created by Alexandr on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DancerMusicToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
